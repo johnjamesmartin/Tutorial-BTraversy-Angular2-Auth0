624817268ff366d4d128eaa847c6fef6982188bc
“John <johnjamesmartin@live.com>
2016-10-24 20:22:11 +0100

    Initial commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HomeComponent, ProfileComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
